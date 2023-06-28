0416210a5c18df0af1a2fa75263b1b912817181e
Roosta078 <tommyagnello78@gmail.com>
2023-06-28 18:32:35 -0400

    Fix board not detected after unplug replug

---
+++ firmware/code/ploopy_headphones.h
@@ -0,0 +1,29 @@
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * This header describes the board, as found in pico-sdk/src/boards/include/.  
+ * The configuration is based off of the Pico and any defined macros here 
+ * will not be overwritten by the base header file
+ */
+
+#ifndef _PLOOPY_HEADPHONES_H
+#define _PLOOPY_HEADPHONES_H
+
+#define PICO_FLASH_SPI_CLKDIV 4
+
+#include "boards/pico.h"
+
+#endif //_PLOOPY_HEADPHONES_H
